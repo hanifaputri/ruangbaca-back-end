70aff471dab83c57f0380b751ebc0b84a38b1d3e
Ghina Zahirah <ghinzahirah@gmail.com>
2021-11-22 15:46:16 +0700

    create controller & database migration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,5 +14,112 @@ class UserController extends Controller
         //
     }
 
-    // TODO: Create user logic
+   public function index(Request $request)
+    {
+        $user = User::all();
+
+        if ($user->isEmpty()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'There are no users.',
+            ], 200);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Users are listed.',
+            'data' => [
+                'user' => $user,
+            ],
+        ], 200);
+    }
+
+    public function show(Request $request, $idUser)
+    {
+        $user = User::find($idUser);
+
+        // No data user
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found.',
+            ], 404);
+        }
+
+        // User 'Admin' is found
+        if($request->user->hasRole('admin')) {
+            return response()->json([
+                'success' => true,
+                'message' => 'User is found.',
+                'data' => [
+                    'user' => $user,
+                ],
+            ], 200);
+        }
+
+        // Access denial
+        if ($user->email != $request->user->email) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid data.',
+            ], 403);
+        }
+
+        // Other role is found
+        return response()->json([
+            'success' => true,
+            'message' => 'User is found',
+            'data' => [
+                'user' => $user,
+            ],
+        ], 200);
+    }
+
+    public function update(Request $request, $idUser)
+    {
+        $user = User::find($idUser);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        if ($user->email != $request->user->email) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid data.',
+            ], 403);
+        }
+
+        $user->update($request->all());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data user has been updated!',
+            'data' => [
+                'user' => $user,
+            ],
+        ], 200);
+    }
+
+    public function destroy(Request $request, $idUser)
+    {
+        $user = User::find($idUser);
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found.',
+            ], 404);
+        }
+
+        $user->delete($idUser);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data user has been deleted.',
+        ], 200);
+    }
 }
--- database/migrations/2021_11_12_103501_create_users.php
+++ database/migrations/2021_11_12_103501_create_users.php
@@ -15,7 +15,9 @@ class CreateUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            // This is where you type your code
+            $table->string('name');
+            $table->string('email');
+            $table->string('password');
 
             $table->enum('role', ['user', 'admin']);
             $table->timestamps();
