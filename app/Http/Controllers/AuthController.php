554db571ceda19fec568a986d66a2ce8f0119c74
Alizza Iman <alizzaimanraddin@gmail.com>
2021-11-28 16:48:26 +0700

    Insert Syntax for AuthTest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use Firebase\JWT\JWT;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
@@ -37,12 +39,24 @@ class AuthController extends Controller
         $password = Hash::make($request->input('password'));
         $role = $request->input('role');
 
-        $validated = $this->validate($request, [
+       /* $validated = $this->validate($request, [
             'name'      => 'required',
             'email'     => 'unique:users|required|email',
             'password'  => 'required',
             'role'      => 'in:Admin,User|required'
+        ]);*/
+
+        $val_required = Validator::make($request->all(),[
+            'name'      => 'required',
+            'email'     => 'unique:users|required|email',
+            'password'  => 'required',
         ]);
+
+        if ($val_required->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'Field should not be empty',
+        ],400);}
 
         try {
             $register = User::create([
@@ -51,13 +65,19 @@ class AuthController extends Controller
                 'password'  => $password,
                 'role'      => $role
             ]);
+            $user = User::where('email', $request->email)->first();
+            if ($user){
+                return response()->json([
+                    'success'=>false,
+                    'message'=>'Email already existed'
+                ], 400);
+            };
             if($register){
                 return response()->json([
                     'success' => true,
                     'message' => 'Register Success!',
-                    'data' => ['token'=>$this->jwt($register),
-                ]
-            ], 201);
+                    'data' => ['token'=>$this->jwt($register),]
+                ], 201);
             } else {
                 return response()->json([
                     'success' => false,
@@ -79,19 +99,25 @@ class AuthController extends Controller
         ]);
         try {
             $user = User::where('email', $request->email)->first();
+            if ($user){
+                return response()->json([
+                    'success'=>false,
+                    'message'=>'Email not found!'
+                ], 404);
+            };
             if (Hash::check($request->password,$user->password)){
                 return response()->json([
                     'success'=>true,
-                    'message'=>'Successfully Logged in',
+                    'message'=>'Successfully Logged in!',
                     'data'=>[
                         'token'=>$this->jwt($user)
                     ]
-                ]);
+                ],200);
             } else {
                 return response()->json([
                     'success'=>false,
-                    'message'=>'Wrong Password!'
-                ]);
+                    'message'=>'Credential not match!'
+                ], 400);
             };
         } catch(\Exception $e) {
             return response()->json([
